d81b821df2d1fa6b33345aed950f5c2b53b14772
Tuba Hosseini <tubahosseini@gmail.com>
2024-05-04 17:46:47 +0330

    add Routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
